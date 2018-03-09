7052b1a5d8d55d56c80c5ac2317798420fc7e517
Qiaoyu Zhang <owenqyzhang@uses.noreply.github.com>
2018-03-08 23:12:09 -0500

    Added initializer.

---
+++ Examples/kitti.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
+#include <iomanip>
+
+// #include <opencv2/core/core.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    
+}
